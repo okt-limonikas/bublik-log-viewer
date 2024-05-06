f8be04f9b447f0537614f7c690aad8a780d560d6
Danil Kostromin <danil.kostromin@arknetworks.am>
2024-05-06 04:42:24 +0300

    Remove
    Signed-off-by: Danil Kostromin <danil.kostromin@arknetworks.am>

--- constants/constants.go
+++ constants/constants.go
@@ -1,6 +1,5 @@
 package constants
 
-const BIN_NAME string = "bublik-log-viewer"
-const GIT_URL string = "https://api.github.com/repos/okt-limonikas/bublik-log-viewer/releases/latest"
+const BIN_NAME = "bublik-log-viewer"
+const GIT_URL = "https://api.github.com/repos/okt-limonikas/bublik-log-viewer/releases/latest"
 const BUILD_PATH = "build"
-
